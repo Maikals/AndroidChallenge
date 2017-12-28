1f2900bbdf514a9166bc22cd4cccf4da55832bdd
Miquel Castanys <miquicm@gmail.com>
2017-12-28 21:02:56 +0100

    Added error empty view

--- app/src/main/java/com/example/miquelcastanys/androidchallenge/presentation/enumerations/EmptyViewModel.kt
+++ app/src/main/java/com/example/miquelcastanys/androidchallenge/presentation/enumerations/EmptyViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.miquelcastanys.androidchallenge.presentation.enumerations
 
+import com.example.miquelcastanys.androidchallenge.R
+
 
 enum class EmptyViewModel(var imageId: Int, var title: Int, var subtitle: Int) {
-
+    ERROR(R.drawable.github, R.string.error_empty_view_title, R.string.error_empty_view_description)
 }
